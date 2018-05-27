4a382d5504044806d0ab99fde0193bf1b6bdf66b
d833357 <saiviswanathvenkatkris.palaparthi@team.telstra.com>
2018-05-27 15:46:09 +0000

    New

--- couchbase/configure-node.sh
+++ couchbase/configure-node.sh
@@ -7,7 +7,7 @@ set -m
 
 sleep 15
 
-curl -X POST -u Administrator:password -d "name=default1" -d "ramQuotaMB=20"0 -d "authType=none" -d "replicaNumber=0" -d "proxyPort=11216" "http://127.0.0.1:8091/pools/default/buckets"
+curl -X POST -u Administrator:password -d "name=default" -d "ramQuotaMB=20"0 -d "authType=none" -d "replicaNumber=0" -d "proxyPort=11216" "http://127.0.0.1:8091/pools/default/buckets"
 
 
 fg 1
