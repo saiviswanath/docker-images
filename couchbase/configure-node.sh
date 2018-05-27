d6eda700df692c5101be87d51e7f1ef73d06fc84
d833357 <saiviswanathvenkatkris.palaparthi@team.telstra.com>
2018-05-27 15:40:51 +0000

    new

--- couchbase/configure-node.sh
+++ couchbase/configure-node.sh
@@ -7,37 +7,7 @@ set -m
 
 sleep 15
 
-# Setup index and memory quota
-curl -v -X POST http://127.0.0.1:8091/pools/default -d memoryQuota=300 -d indexMemoryQuota=300
+curl -X POST -u Administrator:password -d "name=default1" -d "ramQuotaMB=20"0 -d "authType=none" -d "replicaNumber=0" -d "proxyPort=11216" "http://127.0.0.1:8091/pools/default/buckets"
 
-# Setup services
-curl -v http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex
-
-# Setup credentials
-curl -v http://127.0.0.1:8091/settings/web -d port=8091 -d username=Administrator -d password=password
-
-# Setup Memory Optimized Indexes
-curl -i -u Administrator:password -X POST http://127.0.0.1:8091/settings/indexes -d 'storageMode=memory_optimized'
-
-# Load travel-sample bucket
-#curl -v -u Administrator:password -X POST http://127.0.0.1:8091/sampleBuckets/install -d '["travel-sample"]'
-
-echo "Type: $TYPE"
-
-if [ "$TYPE" = "WORKER" ]; then
-  echo "Sleeping ..."
-  sleep 15
-
-  #IP=`hostname -s`
-  IP=`hostname -I | cut -d ' ' -f1`
-  echo "IP: " $IP
-
-  echo "Auto Rebalance: $AUTO_REBALANCE"
-  if [ "$AUTO_REBALANCE" = "true" ]; then
-    couchbase-cli rebalance --cluster=$COUCHBASE_MASTER:8091 --user=Administrator --password=password --server-add=$IP --server-add-username=Administrator --server-add-password=password
-  else
-    couchbase-cli server-add --cluster=$COUCHBASE_MASTER:8091 --user=Administrator --password=password --server-add=$IP --server-add-username=Administrator --server-add-password=password
-  fi;
-fi;
 
 fg 1
